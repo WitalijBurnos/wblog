bd4e895f14d08d96911cea6ae150b15289ec8186
Burnos Witalij <burnos.witalik@gmail.com>
2024-03-07 22:56:16 +0100

    first step complete

---
+++ wback/src/main/java/com/wback/wback/WbackApplication.java
@@ -0,0 +1,13 @@
+package com.wback.wback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbackApplication.class, args);
+	}
+
+}
